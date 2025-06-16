9f64b56723643b4f77cc3cacea8f1bb824eeb85d
Mariya Lysenkova Wiklander <mariya.lysenkova@medsci.uu.se>
2025-06-16 10:51:20 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Define the pages
+about = st.Page("about.py", title="About", icon="🎈")
+predict = st.Page("predict.py", title="Predict", icon="❄️")
+
+# Set up navigation
+pg = st.navigation([about, predict])
+
+# Run the selected page
+pg.run()
