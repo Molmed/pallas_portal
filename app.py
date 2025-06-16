8090fd4700321132bb7dee826556ffcb846a743a
Mariya Lysenkova Wiklander <mariya.lysenkova@medsci.uu.se>
2025-06-16 15:08:25 +0200

    Add team page

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import streamlit as st
 # Define the pages
 about = st.Page("about.py", title="About", icon="🧭")
 predict = st.Page("predict.py", title="Predict", icon="🔮")
+team = st.Page("team.py", title="Team", icon="🧑‍🔬")
 
 # Set up navigation
-pg = st.navigation([about, predict])
+pg = st.navigation([about, predict, team])
 
 # Run the selected page
 pg.run()
