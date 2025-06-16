0a8a271f66e671b9bbfc573a14622116c9ddb9c8
Mariya Lysenkova Wiklander <mariya.lysenkova@medsci.uu.se>
2025-06-16 11:35:26 +0200

    Add basic components to web app

--- predict.py
+++ predict.py
@@ -1,16 +1,37 @@
 import streamlit as st
 import time
 
-# 'Starting a long computation...'
 
-# # Add a placeholder
-# latest_iteration = st.empty()
-# bar = st.progress(0)
+def show_progress():
+    'Starting a long computation...'
 
-# for i in range(100):
-#   # Update the progress bar with each iteration.
-#   latest_iteration.text(f'Iteration {i+1}')
-#   bar.progress(i + 1)
-#   time.sleep(0.1)
+    # Add a placeholder
+    latest_iteration = st.empty()
+    bar = st.progress(0)
 
-# '...and now we\'re done!'
+    for i in range(100):
+        # Update the progress bar with each iteration.
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i + 1)
+        time.sleep(0.1)
+
+    '...and now we\'re done!'
+
+
+def render_form(id, uploader_label):
+    form = st.form(id, border=False)
+    upload = form.file_uploader(uploader_label, type=["csv", "tsv", "txt"])
+    slider = form.slider("Error tolerance", 0, 100, 90)
+
+    # Every form must have a submit button.
+    submitted = form.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider)
+        show_progress()
+
+
+gex_tab, dnam_tab = st.tabs(["Gene Expresssion", "DNA Methylation"])
+with gex_tab:
+    render_form("gex_form", "Upload Gene Expression Data")
+with dnam_tab:
+    render_form("dnam_form", "Upload DNA Methylation Data")
