1b083e416aa567cc1fa99905f9146ed4a257b252
Mariya Lysenkova Wiklander <mariya.lysenkova@medsci.uu.se>
2025-06-16 15:05:51 +0200

    Flesh out about page

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 # Define the pages
-about = st.Page("about.py", title="About", icon="🎈")
-predict = st.Page("predict.py", title="Predict", icon="❄️")
+about = st.Page("about.py", title="About", icon="🧭")
+predict = st.Page("predict.py", title="Predict", icon="🔮")
 
 # Set up navigation
 pg = st.navigation([about, predict])
