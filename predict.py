5d3b663fb66f34bd988042534495351930deadeb
Mariya Lysenkova Wiklander <mariya.lysenkova@medsci.uu.se>
2026-09-04 14:04:24 +0200

    Generate upset plot

--- predict.py
+++ predict.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import time
-import requests
 import pandas as pd
 from mlflow_client import MlflowClient
+import matplotlib.pyplot as plt
+from upsetplot import plot
 
 
 if "mlflow" not in st.session_state:
@@ -14,6 +14,51 @@ mlflow_models = st.session_state.mlflow_models
 
 # Order models as follows: all models containing "latest" first
 mlflow_models = sorted(mlflow_models, key=lambda x: "latest" in x, reverse=True)
+
+
+def upset_plot(predictions, color="#1f77b4"):
+    raw_predictions = predictions["prediction"].tolist()
+
+    prediction_sets = [
+        set(value) if isinstance(value, (list, tuple, set, frozenset))
+        else {value}
+        for value in raw_predictions
+    ]
+
+    class_names = sorted(
+        {label for values in prediction_sets for label in values},
+        key=str,
+    )
+
+    if not class_names:
+        st.info("No prediction classes available to plot.")
+        return
+
+    upset_data = pd.DataFrame(
+        [
+            {class_name: class_name in values for class_name in class_names}
+            for values in prediction_sets
+        ],
+        dtype=bool,
+    )
+
+    upset_data = upset_data.loc[:, upset_data.any(axis=0)]
+    upset_data.set_index(upset_data.columns.tolist(), inplace=True)
+
+    fig = plt.figure(figsize=(8, 3))
+    plot(
+        upset_data,
+        fig=fig,
+        element_size=15,
+        sort_by="cardinality",
+        facecolor=color,
+        show_counts="%d",
+        show_percentages="{:.0%}",
+    )
+    plt.tight_layout()
+
+    st.pyplot(fig)
+    plt.close(fig)
 
 
 def render_form(id, uploader_label):
@@ -39,6 +84,7 @@ def render_form(id, uploader_label):
                 labels = pd.read_csv(labels_upload, index_col=0)
             predictions = mlflow_client.predict(model, data, slider, labels=labels)
             st.write(predictions)
+            upset_plot(predictions)
         else:
             st.error("Please upload a file.")
 
