aecb62b6207a4ec22f611de402d4c0df90b3b28c
Mariya Lysenkova Wiklander <mariya.lysenkova@medsci.uu.se>
2026-09-05 19:49:45 +0200

    Add info page content

--- about.py
+++ about.py
@@ -2,6 +2,12 @@ import streamlit as st
 
 st.title("PALLAS Portal")
 
-"PALLAS10k is an acute leukemia classifier trained on a dataset of over 10,000 Gene Expression and DNA methylation samples."
+st.write(
+    "PALLAS10k is an acute leukemia classifier trained on a dataset "
+    "of over 10,000 Gene Expression and DNA methylation samples."
+)
 
-"Contact mariya.lysenkova@medsci.uu.se for information on how to pre-process your datasets for inference using the PALLAS Portal."
+st.write(
+    "Contact mariya.lysenkova@medsci.uu.se for information on how to "
+    "pre-process your datasets for inference using the PALLAS Portal."
+)
